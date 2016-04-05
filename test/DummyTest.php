9abe2a6313a2eefc2020ff5ef1c413a19ac8d9dd
Niklas Keller <me@kelunik.com>
2016-04-05 17:05:09 +0200

    Init with repository meta

---
+++ test/DummyTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Kelunik\Bugs;
+
+class DummyTest extends \PHPUnit_Framework_TestCase {
+    /** @test */
+    public function doNothing() {
+        $this->assertTrue(true);
+    }
+}
